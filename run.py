3d4127435dc91de4947288e8b02dfe9654947586
Yuvneet Sapra <2823416.cse-aiml@pietgroup.co.in>
2026-06-10 12:18:02 +0530

    server correction

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ def _get_app():
 
 
 if __name__ == "__main__":
-    host = os.getenv("HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", "8000"))
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", "8080"))
     dev_reload = os.getenv("DEV_RELOAD", "false").strip().lower() in ("1", "true", "yes")
 
     app = _get_app()
